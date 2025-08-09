d27f43cabae0eb16722758006fbc5ecf6ec8d4f7
SOFIRITARI AMGBARA <sofiriamgbara@gmail.com>
2025-08-09 11:24:05 +0100

    Update Home component styling to include a white background

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,10 @@
 import Hero from "./components/hero";
 import Footer from "./components/footer";
+import { JSX } from "react";
 
 export default function Home(): JSX.Element {
   return (
-    <main className="min-h-screen pt-[107px] md:pt-[150px] lg:pt-[197px] flex flex-col">
+    <main className="min-h-screen pt-[107px] md:pt-[150px] bg-white lg:pt-[197px] flex flex-col">
       <div className="flex-1">
         <Hero />
       </div>
