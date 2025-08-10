442322648197c1007c0662f14d0d4150b9eeaec3
Sofiritari Amgbara <38821635+swissjake@users.noreply.github.com>
2025-08-10 03:14:56 +0100

    Update icons.tsx

--- apps/web/app/components/icons.tsx
+++ apps/web/app/components/icons.tsx
@@ -1,5 +1,8 @@
+import * as React from "react";
+
+type IconProps = React.SVGProps<SVGSVGElement>;
 export const Icons = {
-  Github: (props: React.SVGProps<SVGSVGElement>) => {
+  Github: (props: IconProps) => {
     return (
       <svg viewBox="0 0 24 24" fill="none" {...props}>
         <path
@@ -12,7 +15,7 @@ export const Icons = {
     );
   },
 
-  npm: (props: React.SVGProps<SVGSVGElement>) => {
+  npm: (props: IconProps) => {
     return (
       <svg viewBox="0 0 24 24" fill="currentColor" {...props}>
         <path d="M1.763 0C.786 0 0 .786 0 1.763v20.474C0 23.214.786 24 1.763 24h20.474c.977 0 1.763-.786 1.763-1.763V1.763C24 .786 23.214 0 22.237 0H1.763zM5.13 5.323l13.837.019-.009 13.836h-3.464l.01-10.382h-3.456L12.04 19.17H5.113l.017-13.847z" />
